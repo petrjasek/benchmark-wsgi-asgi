53bf4b0ac3f49e5999ac16aac5bbae38dbd6e54e
Petr Jasek <jasekpetr@gmail.com>
2021-04-01 08:01:52 +0200

    add mongo query

--- asgi.py
+++ asgi.py
@@ -1,17 +1,24 @@
 import fastapi
 import elasticsearch
+import motor.motor_asyncio
 
 
 app = fastapi.FastAPI()
 es = elasticsearch.AsyncElasticsearch("http://localhost:9201")
+mongo = motor.motor_asyncio.AsyncIOMotorClient()
 
 
 @app.get("/")
 async def home():
+    token = await mongo["superdesk"]["auth"].find_one()
+
     resp = await es.search(
         index="superdesk_archive",
         body={"query": {"match_all": {}}},
         size=20,
     )
 
-    return {"resp": resp}
+    return {
+        "resp": resp,
+        "token": str(token["_id"]),
+    }
--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,24 @@
 import flask
+import pymongo
 import elasticsearch
 
 
 app = flask.Flask(__name__)
 es = elasticsearch.Elasticsearch("http://localhost:9201")
+mongo = pymongo.MongoClient()
 
 
 @app.route('/')
 def home():
+    token = mongo["superdesk"]["auth"].find_one()
+
     resp = es.search(
         index="superdesk_archive",
         body={"query": {"match_all": {}}},
         size=20,
     )
 
-    return {"resp": resp}
+    return {
+        "resp": resp,
+        "token": str(token["_id"]),
+    }
